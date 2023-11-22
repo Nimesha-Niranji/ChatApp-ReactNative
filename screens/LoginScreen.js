fc687520aa17c0dc70153c7e3679e770c578c5de
Nimesha-Niranji <kanathegedaranimeshaniranji@gmail.com>
2023-11-22 15:39:33 +0530

    Create login screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,51 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import {Input, Button} from '@rneui/base';
+import React, {useState} from 'react'
+import { Text, View , StyleSheet} from 'react-native'
 
-const LoginScreen = () => {
+
+const LoginScreen = ({navigation}) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+   
+
   return (
-    <View>
-        <Text>Login Screen</Text>  
+    <View style={styles.container}>
+        <Input 
+            placeholder="Enter Your Email"  
+            value = {email}
+            label="Email"
+            leftIcon={{type:'material',name: 'email'}}
+            onChangeText={text => setEmail(text)} />
+
+        <Input 
+            placeholder="Enter Your Password"  
+            value = {password}
+            label="Password"
+            leftIcon={{type:'material',name: 'lock'}}
+            onChangeText={text => setPassword(text)}
+            secureTextEntry />
+
+        <Button title='Sign in' style={styles.button}/>
+        <Button title='Register' style={styles.button}
+            onPress={() => navigation.navigate('Register')} />
+         
     </View>
   )
 }
+const styles = StyleSheet.create({
+  
+    button:{
+      width: 400,
+      marginTop: 20,
+      color:'red'
+    },
+    container:{
+        flex:1,
+        alignItems: 'center',
+        padding:10
+    }
+  });
 
 export default LoginScreen
+
+
