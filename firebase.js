4351b08a0911baba1225b5309b0b410aa5e64bbc
Nimesha-Niranji <kanathegedaranimeshaniranji@gmail.com>
2023-11-22 17:20:26 +0530

    Add firebase to the project

---
+++ firebase.js
@@ -0,0 +1,25 @@
+import * as firebase from 'firebase';
+import 'firebase/auth';
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBo57-LSW4_qcSKwTQsVMygnq3u9JFIagM",
+    authDomain: "gifted-chat-39bd5.firebaseapp.com",
+    projectId: "gifted-chat-39bd5",
+    storageBucket: "gifted-chat-39bd5.appspot.com",
+    messagingSenderId: "537305525663",
+    appId: "1:537305525663:web:eb3c695385612008f961aa"
+  };
+
+  let app;
+
+  if(firebase.apps.lenth === 0){
+    app = firebase.initializeApp(firebaseConfig);
+  }else{
+    app = firebase.app();
+  }
+
+  const db = app.firestore();
+  const auth = firebase.auth();
+
+  export {db, auth};
