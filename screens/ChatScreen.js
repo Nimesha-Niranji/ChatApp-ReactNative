3981e2728acbff929da7559806df01828a1f8899
Nimesha-Niranji <kanathegedaranimeshaniranji@gmail.com>
2023-11-29 10:57:20 +0530

    create signOut function

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,7 +1,44 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useLayoutEffect } from 'react' 
+import { View, Text, TouchableOpacity } from 'react-native'
+import {auth} from '../firebase'
+import { AntDesign } from '@expo/vector-icons';
+import { Avatar } from '@rneui/base';
 
-const ChatScreen = () => {
+const ChatScreen = ({navigation}) => {
+
+  const signOut = () => {
+    auth.signOut().then(() => {
+      // Sign-out successful.
+      navigation.replace('Login');
+    }).catch((error) => {
+      // An error happened.
+    });
+  };
+  
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <View style={{marginLeft:20}}>
+          <Avatar
+            rounded
+            source = {{
+              uri: auth?.currentUser?.photoURL || 'https://www.seekpng.com/png/detail/115-1150053_avatar-png-transparent-png-royalty-free-default-user.png'
+            }}/>
+        </View>
+      ),
+
+      headerRight: () => (
+        <TouchableOpacity style={{
+          marginRight:30
+        }}
+        onPress={signOut}
+        >
+          <AntDesign name="logout" size={24} color="black" />
+        </TouchableOpacity>
+      ),
+    })
+  },[])
+
   return (
     <View>
       <Text>Chat Screen</Text>
